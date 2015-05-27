58b3cef38cc2077cd9a4abb3f7b863a19e4858b6
Filip Smets <alemaaltevinden@gmail.com>
2015-05-27 15:51:54 +0200

    Fixed parsing errors because of bad format

--- DFAtoRE/Files/parsers.cpp
+++ DFAtoRE/Files/parsers.cpp
@@ -4,103 +4,101 @@
 #include <string>
 #include <vector>
 
-// #include "./TinyXML/tinyxml.h"
+#include "./TinyXML/tinyxml.h"
 #include "nodes.h"
 
 using namespace std;
 
 // Returns a vector of addresses from nodes
 // Nodes have to be deleted when no longer required
-/*
-vector<Node*> nodesFromXMLFile (const char* filename) {
-	// Create xml dom
-	TiXmlDocument doc(filename);
+// vector<Node*> nodesFromXMLFile (const char* filename) {
+// 	// Create xml dom
+// 	TiXmlDocument doc(filename);
 	
-	// Load the document
-	if (!doc.LoadFile()) {
-		cerr << "File " << filename << " not found" << endl;
-		return vector<Node*> {};
-	}
+// 	// Load the document
+// 	if (!doc.LoadFile()) {
+// 		cerr << "File " << filename << " not found" << endl;
+// 		return vector<Node*> {};
+// 	}
 
-	// Get root element
-	TiXmlElement* root = doc.FirstChildElement();
-	if (root == NULL) {
-		cerr << "XML Error: No root element" << endl;
-		return vector<Node*> {};
-	}
+// 	// Get root element
+// 	TiXmlElement* root = doc.FirstChildElement();
+// 	if (root == NULL) {
+// 		cerr << "XML Error: No root element" << endl;
+// 		return vector<Node*> {};
+// 	}
 
-	// Root element should be 'AUTOMATON'
-	if (string(root->Value()) != "AUTOMATON") {
-		cerr << "XML Error: Root element has to be called 'AUTOMATON' but was '" << root->Value() << "'" << endl;
-		return vector<Node*> {};
-	}
+// 	// Root element should be 'AUTOMATON'
+// 	if (string(root->Value()) != "AUTOMATON") {
+// 		cerr << "XML Error: Root element has to be called 'AUTOMATON' but was '" << root->Value() << "'" << endl;
+// 		return vector<Node*> {};
+// 	}
 
-	TiXmlElement* states = root->FirstChildElement();
-	TiXmlElement* connections = states->NextSiblingElement();
+// 	TiXmlElement* states = root->FirstChildElement();
+// 	TiXmlElement* connections = states->NextSiblingElement();
 
-	vector<Node*> allStates;
+// 	vector<Node*> allStates;
 	
-	// Parse the states
-	for (TiXmlElement* state = states->FirstChildElement(); state != NULL; state = state->NextSiblingElement()) {
-		string begin = state->Attribute("begin");
-		string accept = state->Attribute("accept");
+// 	// Parse the states
+// 	for (TiXmlElement* state = states->FirstChildElement(); state != NULL; state = state->NextSiblingElement()) {
+// 		string begin = state->Attribute("begin");
+// 		string accept = state->Attribute("accept");
 
-		bool beginBool;
-		bool acceptBool;
+// 		bool beginBool;
+// 		bool acceptBool;
 
-		if (begin == "true") {
-			beginBool = true;
-		} else {
-			beginBool = false;
-		}
+// 		if (begin == "true") {
+// 			beginBool = true;
+// 		} else {
+// 			beginBool = false;
+// 		}
 
-		if (accept == "true") {
-			acceptBool = true;
-		} else {
-			acceptBool = false;
-		}
+// 		if (accept == "true") {
+// 			acceptBool = true;
+// 		} else {
+// 			acceptBool = false;
+// 		}
 
-		TiXmlNode* TXnode = state->FirstChild();
-		TiXmlText* text = TXnode->ToText();
-		string statename = text->Value();
+// 		TiXmlNode* TXnode = state->FirstChild();
+// 		TiXmlText* text = TXnode->ToText();
+// 		string statename = text->Value();
 
-		allStates.push_back(new Node(beginBool, acceptBool, statename));
-	}
+// 		allStates.push_back(new Node(beginBool, acceptBool, statename));
+// 	}
 
-	for (TiXmlElement* conEl = connections->FirstChildElement(); conEl != NULL; conEl = conEl->NextSiblingElement()) {
-		string from = conEl->Attribute("from");
-		string to = conEl->Attribute("to");
+// 	for (TiXmlElement* conEl = connections->FirstChildElement(); conEl != NULL; conEl = conEl->NextSiblingElement()) {
+// 		string from = conEl->Attribute("from");
+// 		string to = conEl->Attribute("to");
 
-		TiXmlNode* node = conEl->FirstChild();
-		TiXmlText* text = node->ToText();
-		string label = text->Value();
+// 		TiXmlNode* node = conEl->FirstChild();
+// 		TiXmlText* text = node->ToText();
+// 		string label = text->Value();
 
-		Node* fromState;
-		Node* toState;
+// 		Node* fromState;
+// 		Node* toState;
 
-		for(auto &state : allStates) {
-			if (state->name == from) {
-				fromState = state;
-			}
-			if (state->name == to) {
-				toState = state;
-			}
-		}
+// 		for(auto &state : allStates) {
+// 			if (state->name == from) {
+// 				fromState = state;
+// 			}
+// 			if (state->name == to) {
+// 				toState = state;
+// 			}
+// 		}
 
-		Connection con = Connection(label, fromState, toState);
+// 		Connection con = Connection(label, fromState, toState);
 
-		for(auto &state : allStates) {
-			if (state->name == from) {
-				state->addConnection(con);
-			} else if (state->name == to) {
-				state->addConnection(con);
-			}
-		}
-	}
+// 		for(auto &state : allStates) {
+// 			if (state->name == from) {
+// 				state->addConnection(con);
+// 			} else if (state->name == to) {
+// 				state->addConnection(con);
+// 			}
+// 		}
+// 	}
 
-	return allStates;
-}
-*/
+// 	return allStates;
+// }
 
 vector<Node*> nodesFromDotFile (const char* filename) {
 	std::string name;
@@ -140,17 +138,34 @@ vector<Node*> nodesFromDotFile (const char* filename) {
 				}
 
 				if (!fromAlreadyAdded) {
-					allStates.push_back(new Node(!start_set, false, from));
+					allStates.push_back(new Node(false, false, from));
 					start_set = true;
 				}
 
 				if (!toAlreadyAdded) {
-					allStates.push_back(new Node(!start_set, false, to));
+					allStates.push_back(new Node(false, false, to));
 					start_set = true;
 				}
+
 			} else {
-				std::cerr << "PARSING ERROR: incorrect syntax: " << std::endl << line << std::endl
-				<< "No label found." << std::endl << std::endl;				
+				if (line.find("-1->")) {
+					// stateTo
+					string to = line;
+					to.erase(0, to.find("\"") + 1);
+					to.erase(to.find("\""), to.size());
+
+					for(auto &state : allStates) {
+						if (state->name == to) {
+							state->accept = true;
+							continue;
+						}
+					}
+
+					allStates.push_back(new Node(true, false, to));
+				} else {
+					std::cerr << "PARSING ERROR: incorrect syntax: " << std::endl << line << std::endl
+					<< "No label found." << std::endl << std::endl;
+				}
 			}
 		} else if (line.find("}") != -1) {
 			break;
@@ -164,11 +179,8 @@ vector<Node*> nodesFromDotFile (const char* filename) {
 					state->accept = true;
 				}
 			}
-		} else if (line.find("digraph {") != -1) {
+		} else if (line.find("digraph") != -1) {
 			continue;
-		} else {
-			std::cerr << "PARSING ERROR: incorrect syntax: " << std::endl << line << std::endl
-			<< "No '->' found nor a doublecircle." << std::endl << std::endl;
 		}
 	}
 
@@ -221,8 +233,10 @@ vector<Node*> nodesFromDotFile (const char* filename) {
 				}
 			}
 			else {
-				std::cerr << "PARSING ERROR: incorrect syntax: " << std::endl << line << std::endl
-				<< "No label found." << std::endl << std::endl;				
+				if (!line.find("-1")) {
+					std::cerr << "PARSING ERROR: incorrect syntax: " << std::endl << line << std::endl
+					<< "No label found so I ignored this line." << std::endl << std::endl;	
+				}		
 			}
 		}
 		else if (line.find("}") != -1) {
@@ -230,10 +244,6 @@ vector<Node*> nodesFromDotFile (const char* filename) {
 		}
 		else if (line.find("[shape=\"doublecircle\"]")) {
 			continue;
-		}
-		else {
-			std::cerr << "PARSING ERROR: incorrect syntax: " << std::endl << line << std::endl
-			<< "No '->' found nor a doublecircle." << std::endl << std::endl;
 		}
 	}
 

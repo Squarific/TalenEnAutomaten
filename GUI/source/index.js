490368acd0bd52521782aadb502ae6ce3c62c980
Filip Smets <alemaaltevinden@gmail.com>
2015-06-01 14:00:31 +0200

    Fixed a few bugs with gui

--- GUI/source/index.js
+++ GUI/source/index.js
@@ -1,30 +1,63 @@
 var cmd = require('child_process').exec;
+var graphFiles = [{
+	name: "reToNFA1",
+	file: "step1.gv"
+}, {
+	name: "MSSC1",
+	file: "step2.gv"
+}, {
+	name: "TF",
+	file: "step3.gv"
+}];
+
+var productGraphFiles = [{
+	name: "reToNFA1",
+	file: "step1a.gv"
+}, {
+	name: "reToNFA2",
+	file: "step1b.gv"
+}, {
+	name: "MSSC1",
+	file: "step2a.gv"
+}, {
+	name: "MSSC2",
+	file: "step2b.gv"
+}, {
+	name: "product",
+	file: "step3.gv"
+}, {
+	name: "TF",
+	file: "step4.gv"
+}];
+
+function removeAllChildren (elem) {
+	 while (elem.firstChild) elem.removeChild(elem.firstChild);
+}
 
 function minifyRegex (regex1, regex2) {
+	regex1 = '"' + regex1 + '"';
+	if (regex2) regex2 = '"' + regex2 + '"';
+
 	cmd("./helper/simplify " + regex1 + " " + regex2, function (error, stdout, stderr) {
 		document.getElementById("usedregex").innerText = regex1;
+		document.getElementById("DFAToRegex").innerText = stdout;
+
+		for (var k = 0; k < productGraphFiles.length; k++) {
+			removeAllChildren(document.getElementById(productGraphFiles[k].name));
+			document.getElementById(productGraphFiles[k].name).setAttribute("viewBox", "0 0 0 0");
+		}
 
 		if (regex2 !== "") {
 			document.getElementById("usedregex").innerText += " producted with " + regex2;
+			rerenderGraphs(productGraphFiles);
+		} else {
+			rerenderGraphs(graphFiles);
 		}
-
-		document.getElementById("DFAToRegex").innerText = stdout;
-
-		rerenderGraphs();
 	});
 }
 
-function rerenderGraphs () {
-	var graphFiles = [{
-		name: "step1",
-		file: "step1.gv"
-	}, {
-		name: "step2",
-		file: "step2.gv"
-	}, {
-		name: "step3",
-		file: "step3.gv"
-	}];
+function rerenderGraphs (graphFiles) {
+
 
 	for (var k = 0; k < graphFiles.length; k++) {
 		getGraph(graphFiles[k]);
